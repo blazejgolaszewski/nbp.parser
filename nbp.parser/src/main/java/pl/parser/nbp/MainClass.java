e0d72f14fa6d0702d4dd68472380e1611ae822b8
zedog <zedog@gazeta.pl>
2014-06-23 15:10:44 -0400

    main class rename

---
+++ nbp.parser/src/main/java/pl/parser/nbp/MainClass.java
@@ -0,0 +1,13 @@
+package pl.parser.nbp;
+
+/**
+ * Hello world!
+ *
+ */
+public class MainClass 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
